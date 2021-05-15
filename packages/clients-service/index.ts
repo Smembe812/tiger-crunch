3721a2506044d65d492442a7fc478c586aa2b2f4
Smembe812 <psemberekajr@gmail.com>
2021-05-15 12:46:45 +0200

    clean up on client-service index

--- packages/clients-service/index.ts
+++ packages/clients-service/index.ts
@@ -1,11 +1,8 @@
-// import validators from "./validators"
 import makeClient from "./clients"
 import DataSource from "./datasource"
 import makeUseCases from "./usecases"
 import makeManager from "./client-manager"
 const clientManager = makeManager()
-// import MailManager from './mail'
-// import otpService from './otp-service'
 const client = makeClient()
 const dataSource = new DataSource("clients")
 const useCases = makeUseCases({
