2ce918c28d1a0f2a7d3908bd1f69dbd510b8fb64
Smembe812 <psemberekajr@gmail.com>
2021-05-19 09:46:02 +0200

    auth client setup

--- client.js
+++ client.js
@@ -1,19 +1,24 @@
-var fs = require('fs'); 
-var https = require('https'); 
-var options = { 
-    hostname: 'localhost', 
+const fs = require('fs'); 
+const https = require('https'); 
+const host = 'tiger-crunch.com'
+const options = { 
+    hostname: host, 
     port: 4433, 
     path: '/', 
     method: 'GET', 
     key: fs.readFileSync('client1-key.pem'), 
     cert: fs.readFileSync('client1-crt.pem'), 
-    ca: fs.readFileSync('ca-crt.pem') }; 
-var req = https.request(options, function(res) { 
+    ca: fs.readFileSync('ca-crt.pem') 
+}; 
+
+const req = https.request(options, function(res) { 
     res.on('data', function(data) { 
         process.stdout.write(data); 
     }); 
-}); 
-req.end(); 
+});
+req.end();
 req.on('error', function(e) { 
     console.error(e); 
 });
+const server = https.createServer(options)
+server.listen(options.port, ()=>console.log("server up"))
